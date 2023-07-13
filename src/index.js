82d54d16452f9e81cab92d95e8d53cefd165dba2
BhagatHarsh <93080554+BhagatHarsh@users.noreply.github.com>
2023-07-13 21:19:34 +0530

    updates for queue implementation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-const { Client, IntentsBitField, ActivityType } = require('discord.js');
+const { Client, IntentsBitField, ActivityType, VoiceConnectionStatus, entersState } = require('discord.js');
 const {
     joinVoiceChannel,
     createAudioPlayer,
     createAudioResource,
-    StreamType,
-    AudioPlayerStatus
 } = require('@discordjs/voice');
 const ytdl = require('ytdl-core');
+const ytSearch = require('./ytSearch')
+const regex = /^https?:\/\/(youtu\.be\/|(www\.)?youtube\.com\/(embed|v|shorts)\/)/;
+const musicQueues = new Map();
+
 
 const dotenv = require('dotenv')
 dotenv.config()
 
-const client = new Client({ 
+const client = new Client({
     intents: [
         IntentsBitField.Flags.Guilds,
         IntentsBitField.Flags.GuildMembers,
@@ -29,82 +31,123 @@ client.on('ready', (c) => {
     });
 });
 
-client.on("messageCreate", function (msg) { 
+client.on("messageCreate", function (msg) {
     console.log(msg.content);
-    
-    
 
     if (msg.content === "ping") {
         msg.reply("pong!");
         console.log("pong-ed " + msg.author.username);
-}
-});
-
-
-async function playAudioFromYouTube(interaction, url, player) {
-    const voiceChannel = interaction.member.voice.channel;
-
-    if (!voiceChannel) {
-        return interaction.reply('Join a channel and try again');
     }
-
-    const stream = ytdl(url, { filter: 'audioonly',quality:'highestaudio' });
-
-    const resource = createAudioResource(stream);
-
-    player.play(resource);
-
-    channel = joinVoiceChannel({
-        channelId: voiceChannel.id,
-        guildId: voiceChannel.guild.id,
-        adapterCreator: voiceChannel.guild.voiceAdapterCreator,
-    });
-
-    channel.subscribe(player);
-
-    return interaction.reply(`Playing ${url}`);
-};
-
+});
 
 client.on('interactionCreate', async (interaction) => {
     if (!interaction.isChatInputCommand()) return;
     console.log(interaction.commandName)
 
-    if (interaction.commandName === 'p') {
-        const link = interaction.options.get('link').value;
+    if (interaction.commandName === 'play') {
+        let song = interaction.options.get('link').value;
+        const match = song.match(regex);
 
-        // interaction.reply(`The link is ${num1}`);
+        if (match && match[0] === "youtube") {
+            console.log('Youtube Link provided!')
+        } else {
+            console.log('Query provided')
+            song = await ytSearch(song)
+        }
 
-        if (interaction.member.voice.channel) {
-            let channel = null;
-            const player = createAudioPlayer();
+        let guildQueue = musicQueues.get(interaction.guild.id);
 
-            player.on(AudioPlayerStatus.Idle, () => {
-                if (channel) {
-                    channel.leave();
-                    channel = null;
+        let connection = joinVoiceChannel({
+            channelId: interaction.member.voice.channelId,
+            guildId: interaction.guild.id,
+            adapterCreator: interaction.guild.voiceAdapterCreator,
+        });
+        // If there's no song playing, play the song and create a new queue
+        if (!guildQueue) {
+            let player = createAudioPlayer();
+            player.on('idle', () => {
+                let guildQueue = musicQueues.get(interaction.guild.id);
+                if (guildQueue) {
+                    guildQueue.shift();
+                    if (guildQueue.length > 0) {
+                        let nextSong = guildQueue[0]['song'];
+                        let stream = ytdl(nextSong, { filter: 'audioonly', quality: 'highestaudio' });
+                        let resource = createAudioResource(stream);
+                        player.play(resource);
+                        nextSong.interaction.followUp('Now playing the next song in the queue.');
+                    } else {
+                        connection.destroy();
+                    }
                 }
             });
+            let stream = ytdl(song, { filter: 'audioonly', quality: 'highestaudio' });
+            let resource = createAudioResource(stream);
+            let queue = [{
+                interaction: interaction,
+                song: song,
+                connection: connection,
+            }];
 
-            player.on('error', error => {
-                console.error(`Error: ${error.message} with resource ${error.resource.metadata.url}`);
-            });
-
-            await playAudioFromYouTube(interaction, link, player)
+            musicQueues.set(interaction.guild.id, queue);
+            player.play(resource);
+            connection.subscribe(player);
+            interaction.reply('Now playing your song: ' + song);
         } else {
-            interaction.reply('Join a voice channel then try again!');
+            // If a song is already playing, add the new song to the queue
+            guildQueue.push({
+                song: song,
+                interaction: interaction,
+                connection: connection,
+            });
+            interaction.reply('Your song has been added to the queue.');
         }
     }
-        
+
+    if (interaction.commandName === 'skip') {
+        let guildQueue = musicQueues.get(interaction.guild.id);
+
+        if (!guildQueue || guildQueue.length === 0) {
+            interaction.reply('There are no songs in the queue to skip.');
+        } else {
+            // End the current song
+            let currentSong = guildQueue.shift();
+            console.log("Skipping to" + currentSong)
+            currentSong.connection.destroy();
+
+            // If there's another song in the queue, start playing it
+            if (guildQueue.length > 0) {
+                let player = createAudioPlayer();
+                let nextSong = guildQueue[0]['song'];
+                let stream = ytdl(nextSong, { filter: 'audioonly', quality: 'highestaudio' });
+                let resource = createAudioResource(stream);
+                player.play(resource);
+                currentSong.connection.subscribe(player);
+                nextSong.interaction.followUp('Now playing the next song in the queue.');
+            }
+        }
+    }
+
+
+    if (interaction.commandName === 'search') {
+        const query = interaction.options.get('query').value;
+        const url = await ytSearch(query)
+        interaction.reply(JSON.stringify(url));
+    }
+
+    if (interaction.commandName === 'leave') {
+        const voiceChannel = interaction.member.voice.channel;
+
+        if (!voiceChannel) {
+            return interaction.reply('You are not in a voice channel.');
+        }
+
+        voiceChannel.leave();
+        interaction.reply("Left the voice channel");
+    }
+
     if (interaction.commandName === 'ping') {
         interaction.reply("pong!");
     }
-
-})
-
+});
 
 client.login(process.env.TOKEN);
-
-
-
-
